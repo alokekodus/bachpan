1cea5f50fb506e857607e332222ad87ef3360ece
Alok Deka <alok.deka@ekodus.com>
2022-03-04 17:27:40 +0530

    Lightbox gallery added

--- asset/js/main.js
+++ asset/js/main.js
@@ -14,4 +14,21 @@ $('.testimonial-slider').owlCarousel({
             items:1
         }
     }
-})
+});
+
+// This will create a single gallery from all elements that have class "gallery-item"
+$('.gallery-item').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery:{
+      enabled:true
+    },
+    zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it    
+        duration: 300, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition 
+        opener: function(openerElement) {
+          return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+  });
