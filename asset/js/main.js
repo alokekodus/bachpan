7cf83eb82adea805b8108fe3bf8683e75259cbc5
Alok Deka <alok.deka@ekodus.com>
2022-03-04 15:37:08 +0530

    Testimonial slider added

---
+++ asset/js/main.js
@@ -0,0 +1,17 @@
+$('.testimonial-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items:1,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
